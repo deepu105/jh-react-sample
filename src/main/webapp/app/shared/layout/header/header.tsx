b03acb99dbb3a4f2482afcde34e7b56f328cad42
Deepu K Sasidharan <d4udts@gmail.com>
2017-11-03 17:21:05 +0100

    polish

--- src/main/webapp/app/shared/layout/header/header.tsx
+++ src/main/webapp/app/shared/layout/header/header.tsx
@@ -116,7 +116,7 @@ export class Header extends React.Component<IHeaderProps, { menuOpen: boolean }>
                     <FaUserPlus />
                     <span>Administration</span>
                   </DropdownToggle>
-                  <DropdownMenu right>
+                  <DropdownMenu right style={{ width: '120%' }}>
                     {adminMenuItems}
                   </DropdownMenu>
                 </UncontrolledNavDropdown>
